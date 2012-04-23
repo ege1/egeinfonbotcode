66b51b086e8a2a2b4275e8c4812c2f7e386da213
egeumantis@gmail.com <egeumantis@gmail.com@314930f3-fa5f-3706-d401-7c24d8b35fad>
2012-04-23 17:57:59 +0000

    some minor changes

--- trunk/ege.lua
+++ trunk/ege.lua
@@ -1,9 +1,16 @@
 --
---
+--------------------------------------------------------------------------
+-- ToDo
+--------------------------------------------------------------------------
+-- If food nearby look how much and inform others
+-- if food here, search around
+-- if food here, remember Position
 --------------------------------------------------------------------------
 -- Variables
 --------------------------------------------------------------------------
 --
+--set food coords if food is bigger min_food
+min_food = 5
 --begin heal below that value
 heal_health = 25
 -- be koth only when health is over that
@@ -44,8 +51,6 @@ typ1_min = 1
 typ2_min = 2
 typ2_min_typ1 = 1
 
--- if fleeing, we need to flee more than enemy can reach
-flee_min_range = 1000
 -- predefined
 king = false
 -- reset values, get resettet every reset_wait msecs
@@ -58,7 +63,7 @@ reset_koth_walkable = true
 -- Knowledge
 --------------------------------------------------------------------------
 
--- typ0 
+-- typ0
 -- can kill typ2
 -- can convert to type1 (using 8000 food) or type2 (using 5000 food)
 -- can feed 400
@@ -79,45 +84,60 @@ function Creature:search_food()
     set_message(self.id, "hungry")
 end
 
+-- convert, but decide convert to what
+function Creature:convert()
+ 
+end
+
+-- attack enemy
+function Creature:attack(enemy)
+ 
+end
 -- main for workers
 
 function Creature:main_worker()
     -- get some infos of me
-    local health = get_health()
-    local food = get_food()
+    local health = get_health(self.id)
+    local food = get_food(self.id)
+    local mex,mey = get_pos(self.id)
+    if get_tile_food(self.id) >= min_food then
+        food_koordx = mex
+        food_koordy = mey
+    end
     local state = get_state()
-    local enemyid, enemyx, enemyy, enemynum, enemydist = nearest_enemy(self.id)
+    local enemyid, enemyx, enemyy, enemynum, enemydist = get_nearest_enemy(self.id)
     if king then
-	if king == self.id then
-	    set_message(self.id, "KING")
-	else
-	    local otherking = true
+        if king == self.id then
+            set_message(self.id, "KING")
+        else
+            local otherking = true
+        end
     end
     if get_type(enemyid) not 2 then
-	enemyid = false
+        local enemyid = false
     end
     -- make some decisions
     if health > convert_health and food > convert_food and state not "CREATURE_CONVERT" and state not "CREATURE_ATTACK" then
-	self:convert()
+        self:convert()
     elseif health < heal_health and food > 1 and state not "CREATURE_CONVERT" and state not "CREATURE_ATTACK" then
-	self:heal()
+        self:heal()
     elseif get_tile_food(self.id) > 0 and state not "CREATURE_CONVERT" and state not "CREATURE_ATTACK" then
-	self:eat()
-    elseif enemy and enemydist < typ0_attack_range and state not "CREATURE_CONVERT" and typ0_kill == true then
-	self:attack(enemyid)
+        self:eat()
+    elseif enemyid and enemydist < typ0_attack_range and state not "CREATURE_CONVERT" and typ0_kill == true then
+        self:attack(enemyid)
 -- should we geht koth?
     elseif health > koth_health and koth_walkable and not otherking and state not "CREATURE_CONVERT" and state not "CREATURE_ATTACK" then
-	self:become_koth()
--- something missing?
+        self:become_koth()
+    -- something missing?
     else
-	self:search_food()
+        self:search_food()
     end
 end
 --------------------------------------------------------------------------
 -- Default Code
 --------------------------------------------------------------------------
 
--- Called after the Creature was created. You cannot 
+-- Called after the Creature was created. You cannot
 -- call long-running methods (like moveto) here.
 function Creature:onSpawned(parent)
     if parent then
@@ -135,22 +155,24 @@ function Creature:onAttacked(attacker)
 end
 
 
--- Called by typing 'r' in the console, after creation (after 
--- onSpawned) or by calling self:restart(). No long-running 
+-- Called by typing 'r' in the console, after creation (after
+-- onSpawned) or by calling self:restart(). No long-running
 -- methods calls here!
 function Creature:onRestart()
 --reset some variables all reset_wait msecs
     future = now + reset_wait
     koth_walkable = reset_koth_walkable
+    food_koordx = false
+    food_koordy = false
 end
 
 
--- Called after being killed. Since the creature is already 
+-- Called after being killed. Since the creature is already
 -- dead, self.id cannot be used to call the Lowlevel API.
 function Creature:onKilled(killer)
     if killer == self.id then
         print("Creature " .. self.id .. " suicided")
-    elseif killer then 
+    elseif killer then
         print("Creature " .. self.id .. " killed by Creature " .. killer)
     else
         print("Creature " .. self.id .. " died")
@@ -162,7 +184,7 @@ end
 function Creature:main()
     now = game_time()
     if now >= future then
-	Creature:onRestart()
+    Creature:onRestart()
     end
     local type = get_type(self.id)
     if type == worker then
@@ -175,4 +197,5 @@ function Creature:main()
       print ("FATAL: unknown type " .. type)
     end
 end
---			self:wait_for_next_round()
+--            self:wait_for_next_round()
+
